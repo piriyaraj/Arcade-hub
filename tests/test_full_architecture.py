f372b6a5079731d1660e188e4736f122e1dfd51b
piriyaraj <piriyaraj@gmail.com>
2026-08-07 06:28:35 +0100

    feat: [Cron] Improve exiting game or create new game

--- tests/test_full_architecture.py
+++ tests/test_full_architecture.py
@@ -62,6 +62,7 @@ def test_html_script_and_css_links():
         "cyberapex.html",
         "cybertitan.html",
         "cyberzenith.html",
+        "cyberquantum.html",
         "cybervanguard.html",
         "cybereclipse.html",
         "cyberhorizon.html",
