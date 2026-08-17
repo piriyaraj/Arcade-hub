23b9a749e95387afc5c824d50b6665f5bdf26836
piriyaraj <piriyaraj@gmail.com>
2026-08-17 02:07:24 +0100

    feat: Successfully created and integrated a new game 'Cyber Mirage' (cybermira

---
+++ tests/cybermirage.test.js
@@ -0,0 +1,207 @@
+// Node.js Unit Tests for Cyber Mirage Engine logic
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
+
+// Mock browser globals
+global.window = global.window || {
+  addEventListener: () => {}
+};
+global.document = global.document || {
+  addEventListener: () => {},
+  DOMContentLoaded: 'DOMContentLoaded'
+};
+global.localStorage = {
+  store: {},
+  getItem(key) { return this.store[key] || null; },
+  setItem(key, val) { this.store[key] = val.toString(); },
+  removeItem(key) { delete this.store[key]; }
+};
+
+global.playSound = global.playSound || (() => {});
+
+// Require shared utilities
+const { getBestScore, saveBestScore, checkCollision, checkCircleCollision, clamp, formatScore, randomRange } = require('../utils.js');
+global.getBestScore = getBestScore;
+global.saveBestScore = saveBestScore;
+global.checkCollision = checkCollision;
+global.checkCircleCollision = checkCircleCollision;
+global.clamp = clamp;
+global.formatScore = formatScore;
+global.randomRange = randomRange;
+
+// Extract CyberMirageEngine from cybermirage.html
+const htmlPath = path.join(__dirname, '..', 'cybermirage.html');
+const fileContent = fs.readFileSync(htmlPath, 'utf8');
+
+const startIndex = fileContent.indexOf('class CyberMirageEngine {');
+const endIndex = fileContent.indexOf('if (typeof module !== \'undefined\' && typeof module.exports !== \'undefined\')');
+if (startIndex === -1 || endIndex === -1) {
+  throw new Error('Could not find CyberMirageEngine boundaries in cybermirage.html');
+}
+
+const engineCode = fileContent.substring(startIndex, endIndex) + '\nmodule.exports = { CyberMirageEngine };';
+const evalFn = new Function('module', 'exports', 'checkCollision', 'checkCircleCollision', 'getBestScore', 'saveBestScore', 'clamp', 'formatScore', 'randomRange', engineCode);
+const mockModule = { exports: {} };
+evalFn(mockModule, mockModule.exports, checkCollision, checkCircleCollision, getBestScore, saveBestScore, clamp, formatScore, randomRange);
+
+const CyberMirageEngine = mockModule.exports.CyberMirageEngine;
+
+function createMockCanvas() {
+  return {
+    width: 800,
+    height: 600,
+    getContext: () => ({
+      clearRect: () => {},
+      beginPath: () => {},
+      arc: () => {},
+      fill: () => {},
+      stroke: () => {},
+      moveTo: () => {},
+      lineTo: () => {},
+      closePath: () => {},
+      fillRect: () => {},
+      strokeRect: () => {},
+      fillText: () => {},
+      save: () => {},
+      restore: () => {},
+      translate: () => {},
+      rotate: () => {},
+      setLineDash: () => {},
+      shadowColor: '',
+      shadowBlur: 0,
+      fillStyle: '',
+      strokeStyle: '',
+      lineWidth: 1,
+      globalAlpha: 1,
+      font: '',
+      textAlign: ''
+    }),
+    getBoundingClientRect: () => ({ left: 0, top: 0, width: 800, height: 600 }),
+    addEventListener: () => {}
+  };
+}
+
+test('CyberMirageEngine - Initial state', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+
+  assert.strictEqual(engine.score, 0);
+  assert.strictEqual(engine.wave, 1);
+  assert.strictEqual(engine.started, false);
+  assert.strictEqual(engine.over, false);
+  assert.strictEqual(engine.paused, false);
+  assert.strictEqual(engine.player.hp, 100);
+  assert.strictEqual(engine.player.empCharges, 3);
+  assert.strictEqual(engine.player.energy, 100);
+});
+
+test('CyberMirageEngine - Start game & enemy spawning', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+
+  engine.start();
+  assert.strictEqual(engine.started, true);
+  assert.strictEqual(engine.over, false);
+  assert.strictEqual(engine.score, 0);
+  assert.strictEqual(engine.wave, 1);
+  assert.ok(engine.enemies.length > 0);
+});
+
+test('CyberMirageEngine - Deploy Holographic Decoy', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+  engine.start();
+
+  const initialEnergy = engine.player.energy;
+  engine.triggerDecoy();
+  assert.strictEqual(engine.decoys.length, 1);
+  assert.strictEqual(engine.player.energy, initialEnergy - 20);
+  assert.ok(engine.player.decoyCooldown > 0);
+});
+
+test('CyberMirageEngine - Reflector Shield activation', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+  engine.start();
+
+  const initialEnergy = engine.player.energy;
+  engine.triggerShield();
+  assert.strictEqual(engine.player.shieldActive, true);
+  assert.strictEqual(engine.player.energy, initialEnergy - 25);
+  assert.strictEqual(engine.player.shieldTimer, 300);
+});
+
+test('CyberMirageEngine - EMP Mirage Nova shockwave', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+  engine.start();
+
+  const initialCharges = engine.player.empCharges;
+  engine.triggerEMP();
+  assert.strictEqual(engine.player.empCharges, initialCharges - 1);
+  assert.strictEqual(engine.empWaves.length, 1);
+});
+
+test('CyberMirageEngine - Shooting laser pulse & position update', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+  engine.start();
+
+  engine.mousePos = { x: 500, y: 300 };
+  engine.shootLaser();
+  assert.strictEqual(engine.bullets.length, 1);
+
+  const initialX = engine.bullets[0].x;
+  engine.update();
+  assert.notStrictEqual(engine.bullets[0].x, initialX);
+});
+
+test('CyberMirageEngine - Game pause & resume toggle', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+  engine.start();
+
+  engine.togglePause();
+  assert.strictEqual(engine.paused, true);
+
+  engine.togglePause();
+  assert.strictEqual(engine.paused, false);
+});
+
+test('CyberMirageEngine - Pickup collection & spread upgrade', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+  engine.start();
+
+  engine.applyPickup({ type: 'spread' });
+  assert.strictEqual(engine.player.powerupType, 'spread');
+  assert.ok(engine.player.powerupTimer > 0);
+
+  // Now shootLaser with spread
+  engine.player.lastShot = 0;
+  engine.bullets = [];
+  engine.shootLaser();
+  assert.strictEqual(engine.bullets.length, 3);
+});
+
+test('CyberMirageEngine - Game Over and high score persistence', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+  engine.start();
+
+  engine.score = 6400;
+  engine.endGame();
+
+  assert.strictEqual(engine.over, true);
+  assert.strictEqual(engine.highScore, 6400);
+});
+
+test('CyberMirageEngine - Canvas drawing does not crash', () => {
+  const canvas = createMockCanvas();
+  const engine = new CyberMirageEngine(canvas);
+  engine.start();
+  engine.update();
+  assert.doesNotThrow(() => engine.draw());
+});
